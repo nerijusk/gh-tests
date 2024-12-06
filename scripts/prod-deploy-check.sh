0d3fd9242942da729ae6e26ca16d4579e77c7a34
Nerijus K. <nerijusk@users.noreply.github.com>
2024-12-06 16:34:20 +0200

    2024120616:34:20

---
+++ scripts/prod-deploy-check.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -x
+set -euo pipefail
+
+PATH_LIST=$(mktemp)
+# echo -n "${PATHS}" | jq -r .[] >${PATH_LIST}
+echo -n "${PATHS}" >${PATH_LIST}
+cat ${PATH_LIST}
+PREV_TAG=$(git tag --sort=v:refname | tail -n2 | head -n1)
+LAST_TAG=$(git tag --sort=v:refname | tail -n1)
+echo -e "PREV_TAG: ${PREV_TAG}\nLAST_TAG: ${LAST_TAG}"
+CHANGES_DETECTED=$(git diff --name-only ${PREV_TAG}..${LAST_TAG} | grep -Ef ${PATH_LIST} | wc -l || true)
+if [[ "${CHANGES_DETECTED}" -gt 0 ]]; then
+    echo "deploy_needed=true" >>"${GITHUB_OUTPUT}"
+else
+    echo "deploy_needed=false" >>"${GITHUB_OUTPUT}"
+fi
+rm -f ${PATH_LIST}
