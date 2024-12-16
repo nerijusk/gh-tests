126f53e72528ba4930880c60bbd7337b0aeed035
Nerijus K. <nerijusk@users.noreply.github.com>
2024-12-16 17:32:43 +0200

    2024121617:32:43

--- scripts/deployment-labels.sh
+++ scripts/deployment-labels.sh
@@ -6,13 +6,19 @@ if [[ ${DEBUG} -eq 1 ]]; then
 fi
 set -euo pipefail
 
+BAD_LABELS=""
 case "${PR_ACTION}" in
 "closed" | "reopened" | "synchronize")
     LABEL_LIST=""
     for LABEL in $(echo "${PR_LABELS}" | jq -r .[].name | sed 's/ //g'); do
         if [[ "${LABEL}" =~ ^pr_env:.* ]]; then
             LABEL=$(echo "${LABEL}" | cut -f2 -d:)
-            LABEL_LIST="${LABEL},${LABEL_LIST}"
+            LABEL_CHECK=$(grep --count "${LABEL_NAME}" markets.json)
+            if [[ ${LABEL_CHECK} -eq 0 ]]; then
+                BAD_LABELS="${LABEL_NAME}, ${BAD_LABELS}"
+            else
+                LABEL_LIST="${LABEL},${LABEL_LIST}"
+            fi
         fi
     done
     # in case no labels are found
@@ -23,6 +29,11 @@ case "${PR_ACTION}" in
 "labeled" | "unlabeled")
     if [[ "${PR_LABEL}" =~ ^pr_env:.* ]]; then
         LABEL_NAME=$(echo "${PR_LABEL}" | sed 's/[ "]//g' | cut -f2 -d:)
+        LABEL_CHECK=$(grep --count "${LABEL_NAME}" markets.json)
+        if [[ ${LABEL_CHECK} -eq 0 ]]; then
+            BAD_LABELS="${LABEL_NAME}"
+            LABEL_NAME=""
+        fi
     else
         LABEL_NAME=""
     fi
@@ -32,3 +43,5 @@ esac
 
 # show final label list
 grep label_list ${GITHUB_ENV}
+[[ -n ${BAD_LABELS} ]] && echo "Found invalid labels: ${BAD_LABELS}"
+#  >>${GITHUB_ENV}
