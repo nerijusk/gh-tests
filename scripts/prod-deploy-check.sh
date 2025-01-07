2531946fdc4a494624950974241802f29df829a4
Nerijus K. <nerijusk@users.noreply.github.com>
2025-01-07 18:00:36 +0200

    20250107-1800

--- scripts/prod-deploy-check.sh
+++ scripts/prod-deploy-check.sh
@@ -18,17 +18,17 @@ case "${GITHUB_EVENT_NAME}" in
 "push")
     PREV_TAG=$(git tag --sort=v:refname | tail -n2 | head -n1)
     LAST_TAG=$(git tag --sort=v:refname | tail -n1)
+    echo -e "PREV_TAG: ${PREV_TAG}\nLAST_TAG: ${LAST_TAG}"
+    CHANGES_DETECTED=$(git diff --name-only ${PREV_TAG}..${LAST_TAG} | grep -Ef ${PATH_LIST} | wc -l || true)
     ;;
 "pull_request")
     PREV_TAG=main
-    LAST_TAG=HEAD
+    LAST_TAG=${GITHUB_REF_NAME}
+    echo -e "PREV_TAG: ${PREV_TAG}\nLAST_TAG: ${LAST_TAG}"
+    CHANGES_DETECTED=$(git diff --name-only main | grep -Ef ${PATH_LIST} | wc -l || true)
     ;;
-
 esac
 
-echo -e "PREV_TAG: ${PREV_TAG}\nLAST_TAG: ${LAST_TAG}"
-
-CHANGES_DETECTED=$(git diff --name-only ${PREV_TAG}..${LAST_TAG} | grep -Ef ${PATH_LIST} | wc -l || true)
 echo "CHANGES_DETECTED: ${CHANGES_DETECTED}"
 if [[ "${CHANGES_DETECTED}" -gt 0 ]]; then
     echo "deploy_needed=true" >>"${GITHUB_OUTPUT}"
