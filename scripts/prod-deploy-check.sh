981edfe46894ca3973604b9726d9d7db988a5797
Nerijus K. <nerijusk@users.noreply.github.com>
2025-01-08 10:47:14 +0200

    20250108-1047

--- scripts/prod-deploy-check.sh
+++ scripts/prod-deploy-check.sh
@@ -14,9 +14,6 @@ echo -n "${PATHS}" >${PATH_LIST}
 echo "Paths to check:"
 cat ${PATH_LIST}
 
-# git branch -a
-# git log -n5
-
 case "${GITHUB_EVENT_NAME}" in
 "push")
     PREV_TAG=$(git tag --sort=v:refname | tail -n2 | head -n1)
@@ -25,14 +22,10 @@ case "${GITHUB_EVENT_NAME}" in
 "pull_request")
     PREV_TAG="origin/main"
     LAST_TAG="origin/${GITHUB_HEAD_REF}"
-    # git checkout ${GITHUB_HEAD_REF}
-    # PREV_TAG="remotes/origin/main"
-    # LAST_TAG="remotes/origin/${GITHUB_REF_NAME}"
     ;;
 esac
 
 echo -e "PREV_TAG: ${PREV_TAG}\nLAST_TAG: ${LAST_TAG}\n\nDIFF:"
-git diff --name-only
 git diff --name-only ${PREV_TAG}..${LAST_TAG}
 CHANGES_DETECTED=$(git diff --name-only ${PREV_TAG}..${LAST_TAG} | grep -Ef ${PATH_LIST} | wc -l || true)
 
