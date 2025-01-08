082a4ba87cba6a135ee1f9d95ad6c1f7cab7b965
Nerijus <nerijusk@users.noreply.github.com>
2025-01-08 11:30:04 +0200

    Pr test 01 (#4)
    * 20250107-1744
    
    * 20250107-1744
    
    * 20250107-1750
    
    * 20250107-1755
    
    * 20250107-1800
    
    * 20250108-0932
    
    * 20250108-0936
    
    * 20250108-0939
    
    * 20250108-0943
    
    * 20250108-0953
    
    * 20250108-1002
    
    * 20250108-1015
    
    * 20250108-1017
    
    * 20250108-1047
    
    * 20250108-1048
    
    * 20250108-1050
    
    * 20250108-1104
    
    * 20250108-1106

---
+++ scripts/deploy-check.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+DEBUG=${RUNNER_DEBUG:-0}
+if [[ ${DEBUG} -eq 1 ]]; then
+    set -x
+fi
+set -euo pipefail
+
+PATH_LIST=$(mktemp)
+
+echo -n "${DEPLOYMENT_PATHS}" >${PATH_LIST}
+echo -e "\nPATH_LIST:"
+cat ${PATH_LIST}
+
+case "${GITHUB_EVENT_NAME}" in
+"push")
+    PREV_TAG=$(git tag --sort=v:refname | tail -n2 | head -n1)
+    LAST_TAG=$(git tag --sort=v:refname | tail -n1)
+    ;;
+"pull_request")
+    PREV_TAG="origin/main"
+    LAST_TAG="origin/${GITHUB_HEAD_REF}"
+    ;;
+esac
+
+echo -e "\nPREV_TAG: ${PREV_TAG}\nLAST_TAG: ${LAST_TAG}\n\nDIFF:"
+git diff --name-only ${PREV_TAG}..${LAST_TAG}
+CHANGES_DETECTED=$(git diff --name-only ${PREV_TAG}..${LAST_TAG} | grep -Ef ${PATH_LIST} | wc -l || true)
+
+echo -e "\nCHANGES_DETECTED: ${CHANGES_DETECTED}"
+if [[ "${CHANGES_DETECTED}" -gt 0 ]]; then
+    echo "deploy_needed=true" >>"${GITHUB_OUTPUT}"
+else
+    echo "deploy_needed=false" >>"${GITHUB_OUTPUT}"
+fi
+rm -f ${PATH_LIST}
